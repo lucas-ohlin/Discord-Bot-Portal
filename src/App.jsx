68c39c54f36910c479e3088702e8187d35733337
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2023-11-06 21:09:53 +0100

    Fixed background issue related to width

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css';
 import emcHubImage from './Assets/emchub_icon.png';
 import csImage from './Assets/csgoLogo.png';
 import rustImage from './Assets/rustLogo.png';
+import tarkovImage from './Assets/tarkovLogo.png';
 
 function App() {
 
@@ -30,14 +31,24 @@ function App() {
             invite: 'https://discord.com/api/oauth2/authorize?client_id=920788315551170640&permissions=824633845824&scope=bot%20applications.commands',
         },
         {
+            name: 'TarkovAide',
+            image: tarkovImage,
+            servers: '1 servers',
+            language: 'discord.js',
+            prefix: '/',
+            description: 'Information from an open source project. /help - For commands.',
+            website: 'https://github.com/the-hideout/tarkov-api',
+            invite: 'https://discord.com/api/oauth2/authorize?client_id=1169304274329669643&permissions=824634107968&scope=bot%20applications.commands',
+        },
+        {
             name: 'StatsRust',
             image: rustImage,
             servers: '1 servers',
             language: 'discord.js',
             prefix: '/',
             description: 'Information about players from Rusts API. /help - For commands.',
-            website: 'https://www.counter-strike.net/',
-            invite: 'https://discord.com/api/oauth2/authorize?client_id=920788315551170640&permissions=824633845824&scope=bot%20applications.commands',
+            website: 'https://rust.facepunch.com/',
+            invite: 'https://discord.com/api/oauth2/authorize?client_id=836975617748500520&permissions=824633845824&scope=bot%20applications.commands',
         },
     ]);
 
