bcccff751bd5ec0448be2f2c648262f48b2ccf99
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2024-01-20 17:54:25 +0100

    Added a modal to the More and some basic css to it

--- src/Component/BotCard.jsx
+++ src/Component/BotCard.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../CSS/BotCard.css';
+import { Modal, Button } from 'react-bootstrap';
 
 function BotCard({ bot }) {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     return (
         <div className="bot-card">
             <div className="bot-details">
@@ -10,7 +16,7 @@ function BotCard({ bot }) {
                     <img src={bot.image} alt={bot.name} className="bot-image" />
                     <div className='bot-name-lang'>
                         {bot.name}
-                        <p className="bot-language"><i class="bi bi-code-slash"></i> {bot.language} </p>
+                        <p className="bot-language"><i className="bi bi-code-slash"></i> {bot.language} </p>
                     </div>
                 </div>
                 <p className="bot-description">
@@ -19,9 +25,28 @@ function BotCard({ bot }) {
                 </p>
             </div>
             <div className="bot-actions">
-                <a href={bot.website} target="_blank" rel="noopener noreferrer" className="bot-button">More Info</a>
-                <a href={bot.invite} target="_blank" rel="noopener noreferrer" className="bot-button"><i class="bi bi-download"></i> Invite</a>
+                <Button onClick={handleShowModal} className="bot-button">More Info</Button>
+                <a href={bot.invite} target="_blank" rel="noopener noreferrer" className="bot-button"><i className="bi bi-download"></i> Invite</a>
             </div>
+
+            <Modal show={showModal} onHide={handleCloseModal} className="custom-modal">
+                <Modal.Header>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                            <img src={bot.image} alt={bot.name} className="bot-image" />
+                            <Modal.Title>{bot.name}</Modal.Title>
+                        </div>
+                        <Button onClick={handleCloseModal} className="custom-modal-button">
+                            <i className="bi bi-x-lg"></i>
+                        </Button>
+                    </div>
+                </Modal.Header>
+                <Modal.Body  style={{ justifyContent: 'center' }}>
+                    <p>Data here eventually</p>
+                </Modal.Body>
+                <Modal.Footer style={{ justifyContent: 'center' }}>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
