2806b6cb0a44ad6aec402d03534492e2020c212e
lucas-ohlin <lucas.persson.ohlin@hotmail.com>
2024-04-15 00:27:40 +0200

    Test

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import './index.css'
-import './css/InfiniteScrolling.css'
+import './CSS/InfiniteScrolling.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
